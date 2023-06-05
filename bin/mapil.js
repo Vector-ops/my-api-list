e3e95484f4ad4316914f5bc75d30c76761168a1b
Vector-ops <sumith2827@gmail.com>
2023-06-05 13:29:26 +0530

    build: :tada: setup files
    Setup the initial files for the project. Added all the dependencies.

---
+++ bin/mapil.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const pkg = require("../package.json");
+
+program.version(pkg.version).command("mapil", "Mapil CLI").parse(process.argv);
